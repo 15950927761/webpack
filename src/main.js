65300668b4f08002a50ed88eef825fb364a3c27a
黄建文 <huangjianwen@thmarket.cn>
2019-03-25 16:26:53 +0800

    webpack配置

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import $ from 'jquery'
 import './css/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
+class Person{
+    static info = { name:'zs',age:20 }
+}
 
+console.log(Person.info)
 
 $(function(){
     $("li:odd").css('backgroundColor','lightblue')
