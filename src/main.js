973d40ff49d2e0d258c8aa8c83454c2ec2fb174e
黄建文 <huangjianwen@thmarket.cn>
2019-03-25 16:19:26 +0800

    webpack配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-//main.js是我们项目的js入口文件
-
-//导入jquery
-// import *** from *** 是ES6中导入模块的方式
-//由于es6代码太高级了，所以这一行执行会报错
 import $ from 'jquery'
-// const $ = require('jquery')//node里面引用jquery
+import './css/index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
 
 $(function(){
     $("li:odd").css('backgroundColor','lightblue')
